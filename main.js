2fd8e50ed6857294412f05a2fa4d63b3314729e8
Siddhesh4658 <85446463+Siddhesh4658@users.noreply.github.com>
2022-03-24 15:57:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+img = "";
+status = "";
+objects = [];
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup()
+{
+    canvas = CreateCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    
+    
+    if(status !="")
+
+    for(i = 0; i < objects.length; i++)
+    {
+        document.getElementById("status").innerHTML = "Status: Object Detected";
+
+        fill("#52697d")
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke("#52697d");
+        rect(objects[i].x , objects[i].y , objects[i].height , objects[i].width);
+    }
+}
+
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
